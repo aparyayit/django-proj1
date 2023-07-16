f1d280b0f80ddddd44400568abb0f6ac87d5eec6
itvedant <aparyay.it@gmail.com>
2023-07-16 16:00:12 +0530

    commit-2 done by kajol

--- DeployProj1/deployApp/views.py
+++ DeployProj1/deployApp/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def display(request):
-    s='<h1>Hello World</h1>'
-    return HttpResponse(s)
+    s1='<h1>Hello World</h1>'
+    s2='<h2>Welcome IT Vednat</h2>'
+    return HttpResponse(s1+s2)
