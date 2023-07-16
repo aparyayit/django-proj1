8aaef5237355ac7b237a42440f4f09e36f1df7e5
itvedant <aparyay.it@gmail.com>
2023-07-16 16:44:17 +0530

    changes done by salman

--- DeployProj1/deployApp/views.py
+++ DeployProj1/deployApp/views.py
@@ -4,5 +4,7 @@ from django.http import HttpResponse
 
 def display(request):
     s1='<h1>Hello World</h1>'
-    s2='<h2>Welcome IT Vednat</h2>'
-    return HttpResponse(s1+s2)
+    s2='<h2>Welcome IT Vedantt</h2>'
+    s3='<h3>Good Evening</h3>'
+    s4='<h4>Good Bye</h4>'
+    return HttpResponse(s1+s2+s3)
